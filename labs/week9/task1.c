547d1460b2ce270fedc7bc41a114fefcf951246d
Luyang Liu <luyangl16@gmail.com>
2022-10-03 17:48:16 +1100

    Completed task 5.

--- labs/week9/task1.c
+++ labs/week9/task1.c
@@ -76,7 +76,7 @@ int main(int argc, char *argv[]) {
 	MPI_Cart_shift( comm2D, SHIFT_ROW, DISP, &nbr_i_lo, &nbr_i_hi );
 	MPI_Cart_shift( comm2D, SHIFT_COL, DISP, &nbr_j_lo, &nbr_j_hi );
 
-  printf("%i, %i, %i, %i.\n", nbr_i_lo, nbr_i_hi, nbr_j_lo, nbr_j_hi);
+  /* printf("%i, %i, %i, %i.\n", nbr_i_lo, nbr_i_hi, nbr_j_lo, nbr_j_hi); */
 
 	MPI_Request send_request[4];
   MPI_Request receive_request[4];
@@ -84,9 +84,15 @@ int main(int argc, char *argv[]) {
   MPI_Status receive_status[4];
 
   /* sleep(my_rank); */
-  int randomVal = generate_prime_number(0, 100, my_rank);
+  int randomVal = generate_prime_number(0, 10, my_rank);
   /* unsigned int seed = time(NULL); */
   /* int randomVal = rand_r(&seed) % 100 + 1; */
+
+  int neighbour_ranks[4]  = {nbr_i_lo, nbr_i_hi, nbr_j_lo, nbr_j_hi};
+
+  /* for (int i = 0; i < sizeof(neighbour_ranks)/sizeof(int); i++) { */
+  /*   MPI_Isend(&randomVal, 1, MPI_INT, neighbour_ranks[i], 0, comm2D, &send_request[i]); */
+  /* } */
 
   MPI_Isend(&randomVal, 1, MPI_INT, nbr_i_lo, 0, comm2D, &send_request[0]);
   MPI_Isend(&randomVal, 1, MPI_INT, nbr_i_hi, 0, comm2D, &send_request[1]);
@@ -94,14 +100,28 @@ int main(int argc, char *argv[]) {
   MPI_Isend(&randomVal, 1, MPI_INT, nbr_j_hi, 0, comm2D, &send_request[3]);
 
   int recvValL = -1, recvValR = -1, recvValT = -1, recvValB = -1;
+
+  /* for (int i = 0; i < sizeof(neighbour_ranks) / sizeof(int); i++) { */
+  /*   MPI_Irecv(&recvValT, 1, MPI_INT, neighbour_ranks[i], 0, comm2D, &receive_request[i]); */
+  /* } */
+
+
   MPI_Irecv(&recvValT, 1, MPI_INT, nbr_i_lo, 0, comm2D, &receive_request[0]);
   MPI_Irecv(&recvValB, 1, MPI_INT, nbr_i_hi, 0, comm2D, &receive_request[1]);
   MPI_Irecv(&recvValL, 1, MPI_INT, nbr_j_lo, 0, comm2D, &receive_request[2]);
   MPI_Irecv(&recvValR, 1, MPI_INT, nbr_j_hi, 0, comm2D, &receive_request[3]);
 
+
   printf("%i.\n", send_status[0].MPI_ERROR);
   MPI_Waitall(4, send_request, send_status);
   MPI_Waitall(4, receive_request, receive_status);
+
+  int neighbour_vals[4] = {recvValT, recvValB, recvValL, recvValR};
+  for (int i = 0; i < sizeof(neighbour_ranks) / sizeof(int); i++) {
+    if ( neighbour_vals[i] == randomVal) {
+      printf("rank %i and %i have equal prime number %i.\n", my_rank, neighbour_ranks[i], randomVal);
+    }
+  }
 
   printf("Global rank: %d. Cart rank: %d. Coord: (%d, %d). Random Val: %d. "
          "Recv Top: %d. Recv Bottom: %d. Recv Left: %d. Recv Right: %d.\n",
